08e2fed62950fbf917f2adc7a8796a3c3ed29956
Eric H <eh5311@users.noreply.github.com>
2021-09-29 18:36:53 +0000

    About complex lists

--- advlist/complex01.py
+++ advlist/complex01.py
@@ -6,7 +6,6 @@ list1 = ["cisco_nxos", "arista_eos", "cisco_ios"]
 print(list1)
 # display list1[1] which should only display arista_eos
 print(list1[1])
-# create a new list containing a single item
 list2 = ["juniper"]
 # extend list1 by list2 (combine both lists together into a single list)
 list1.extend(list2)
@@ -22,4 +21,3 @@ print(list1)
 print(list1[4])
 # display the first item in the list (0th item) - first IP address
 print(list1[4][0])
- 
